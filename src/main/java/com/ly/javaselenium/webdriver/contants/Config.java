cdbd0d2dbbec15dcf886b24da46b16a4173a96c6
mx-liyan <65108745+mx-liyan@users.noreply.github.com>
2021-10-28 14:05:26 +0800

    Update Config.java

--- src/main/java/com/ly/javaselenium/webdriver/contants/Config.java
+++ src/main/java/com/ly/javaselenium/webdriver/contants/Config.java
@@ -13,5 +13,5 @@ public class Config {
 
     public static final String passWord0 = "12345";
 
-    public static final String url = "http://192.168.30.65:8081/#/login";
+    public static final String url = "http://192.168.30.65:8080/#/login";
 }
